d08189c0dd8bbbc005e76f35205bc31e9ede0aeb
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-22 22:25:34 +0100

    initial

---
+++ src/Content.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import Mobx from './Mobx.jsx';
+import ReactJS from './ReactJS';
+import Redux from './Redux';
+import TypeScript from './TypeScript';
+
+const Content = () => {
+  return (
+    <main>
+      <Routes>
+        <Route element={<Mobx/>} path="/mobx" />
+        <Route element={<ReactJS/>} path="/react" />
+        <Route element={<Redux/>} path="/redux" />
+        <Route element={<TypeScript/>} path="/typescript" />
+        {/* <Navigate from="*" to="/" /> */}
+      </Routes>
+    </main>
+  );
+};
+
+export default Content;
---
+++ src/Mobx.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mobx = () => {
+  return (
+    <article>
+      <h2>MobX</h2>
+    </article>
+  );
+};
+
+export default Mobx;
