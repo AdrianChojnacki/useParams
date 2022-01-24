9020b10d6d811ca72fc951e90e14a16145147e55
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-24 22:02:18 +0100

    useParams

--- src/Mobx.jsx
+++ src/Mobx.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Mobx = () => {
+  const [inputData, setInputData] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleOnChange = event => setInputData(event.target.value);
+
+  const handleOnClick = () => {
+    navigate(`/typescript#${inputData}`);
+  }
+
   return (
     <article>
       <h2>MobX</h2>
+      <input
+        onChange={handleOnChange}
+        type="text"
+      />
+      <button onClick={handleOnClick}>
+        Wuślij params do TypeScript
+      </button>
     </article>
   );
 };
--- src/TypeScript.jsx
+++ src/TypeScript.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const TypeScript = () => {
+  const paramsObject = useParams();
+
+  console.log(paramsObject);
+
   return (
     <article>
       <h2>TypeScript</h2>
+      <p></p>
     </article>
   );
 };
