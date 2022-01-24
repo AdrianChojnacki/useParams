23bebd3037e6fd24a8709ade3a72c446e9c2d3e0
Michał Zieliński (dev) <michal.zielinski@crafton.pl>
2022-01-24 22:16:38 +0100

    done

--- src/TypeScript.jsx
+++ src/TypeScript.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const TypeScript = () => {
-  const paramsObject = useParams();
-
-  console.log(paramsObject);
+  const { message } = useParams();
 
   return (
     <article>
       <h2>TypeScript</h2>
-      {/* <p>{path}</p> */}
+      <p>{message}</p>
     </article>
   );
 };
